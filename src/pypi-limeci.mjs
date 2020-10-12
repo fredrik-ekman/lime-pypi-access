6607bb29fbca128a392a4394e0da64d2af5fdd42
Johan Andersson <johan.andersson@lime.tech>
2020-10-12 17:22:51 +0200

    feat: add addon repos
    https://github.com/Lundalogik/limepkg-gdpr
    https://github.com/Lundalogik/limepkg-consent
    https://github.com/Lundalogik/addon-erpconnector
    https://github.com/Lundalogik/addon-acd

--- src/pypi-limeci.mjs
+++ src/pypi-limeci.mjs
@@ -1,6 +1,8 @@
 // Add a line for each repository that needs push access to pypi using its name
 // Example: 'limepkg-workorder',
 const repositories = [
+    'addon-acd',
+    'addon-erpconnector',
     'addon-infotiles',
     'addon-lime-event',
     'addon-lime-automation',
@@ -11,10 +13,12 @@ const repositories = [
     'lime-integration-tests',
     'lime-newsletter',
     'lime-project',
+    'limepkg-consent',
     'limepkg-credit-info',
     'limepkg-dynamic-checklist',
     'limepkg-engage',
     'limepkg-esign',
+    'limepkg-gdpr',
     'limepkg-survey-integration',
     'mtech-addon-shared',
     'transactional-message-library',
